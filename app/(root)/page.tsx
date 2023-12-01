c4d7d76e99c5282692c0cfba4b11b9502cb2b549
Filipe <filipe-all@hotmail.com>
2023-12-01 14:48:16 -0300

    Feat: Auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-    return <p>Admin Dashboard</p>;
+export default function Root() {
+    return (
+        <div className="p-4">
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
 }
