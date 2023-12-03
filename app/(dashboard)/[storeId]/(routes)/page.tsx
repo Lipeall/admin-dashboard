a9aee5c12b196ba1fe5dd28c622b0a90c1433b47
Filipe <filipe-all@hotmail.com>
2023-12-03 14:01:50 -0300

    Feat: Navbar

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,16 @@
-const DashboardPage = () => {
-    return <div>Dashboard!</div>;
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps {
+    params: { storeId: string };
+}
+const DashboardPage = async ({ params }: DashboardPageProps) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId,
+        },
+    });
+
+    return <div>Store: {store?.name}</div>;
 };
 
 export default DashboardPage;
