8127c76811e41ab651c50e12289cbf15f5920d77
Filipe <filipe-all@hotmail.com>
2023-12-08 17:14:01 -0300

    Feat: Colors page

--- app/api/[storeId]/colors/[colorId]/route.ts
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -4,36 +4,37 @@ import { NextResponse } from "next/server";
 
 export async function GET(
     _req: Request,
-    { params }: { params: { sizeId: string } }
+    { params }: { params: { colorId: string } }
 ) {
     try {
-        if (!params.sizeId) {
-            return new NextResponse("Size ID is required", {
+        if (!params.colorId) {
+            return new NextResponse("Color ID is required", {
                 status: 400,
             });
         }
 
-        const size = await prismadb.size.findUnique({
+        const color = await prismadb.color.findUnique({
             where: {
-                id: params.sizeId,
+                id: params.colorId,
             },
         });
 
-        return NextResponse.json(size);
+        return NextResponse.json(color);
     } catch (error) {
-        console.log("[SIZE_GET]", error);
+        console.log("[COLOR_GET]", error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
 
 export async function PATCH(
     req: Request,
-    { params }: { params: { storeId: string; sizeId: string } }
+    { params }: { params: { storeId: string; colorId: string } }
 ) {
     try {
         const { userId } = auth();
         const body = await req.json();
         const { name, value } = body;
+        console.log(params.colorId);
 
         if (!userId) {
             return new NextResponse("Unauthenticated", { status: 401 });
@@ -47,8 +48,8 @@ export async function PATCH(
             return new NextResponse("Value is required", { status: 400 });
         }
 
-        if (!params.sizeId) {
-            return new NextResponse("Size ID is required", { status: 400 });
+        if (!params.colorId) {
+            return new NextResponse("Color ID is required", { status: 400 });
         }
 
         const storeByUserId = await prismadb.store.findMany({
@@ -62,9 +63,9 @@ export async function PATCH(
             return new NextResponse("Unauthorized", { status: 403 });
         }
 
-        const size = await prismadb.size.updateMany({
+        const color = await prismadb.color.updateMany({
             where: {
-                id: params.sizeId,
+                id: params.colorId,
             },
             data: {
                 name,
@@ -72,16 +73,16 @@ export async function PATCH(
             },
         });
 
-        return NextResponse.json(size);
+        return NextResponse.json(color);
     } catch (error) {
-        console.log("[SIZE_PATCH]", error);
+        console.log("[COLOR_PATCH]", error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
 
 export async function DELETE(
     _req: Request,
-    { params }: { params: { storeId: string; sizeId: string } }
+    { params }: { params: { storeId: string; colorId: string } }
 ) {
     try {
         const { userId } = auth();
@@ -90,8 +91,8 @@ export async function DELETE(
             return new NextResponse("Unauthenticated", { status: 401 });
         }
 
-        if (!params.sizeId) {
-            return new NextResponse("Size ID is required", {
+        if (!params.colorId) {
+            return new NextResponse("Color ID is required", {
                 status: 400,
             });
         }
@@ -107,15 +108,15 @@ export async function DELETE(
             return new NextResponse("Unauthorized", { status: 403 });
         }
 
-        const size = await prismadb.size.deleteMany({
+        const color = await prismadb.color.deleteMany({
             where: {
-                id: params.sizeId,
+                id: params.colorId,
             },
         });
 
-        return NextResponse.json(size);
+        return NextResponse.json(color);
     } catch (error) {
-        console.log("[SIZE_DELETE]", error);
+        console.log("[COLOR_DELETE]", error);
         return new NextResponse("Internal error", { status: 500 });
     }
 }
